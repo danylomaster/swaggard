c09296f0fd5312e23f05cc42c683f745e35e7773
Sergii Bakus <sergii_bakus@outlook.com>
2018-04-04 17:42:30 +0300

    Revert "fix1"
    This reverts commit 0a7cb9ae62430e265e3242eecfdd6f78e1e0bbf1.

--- app/assets/javascripts/swaggard/application.js
+++ app/assets/javascripts/swaggard/application.js
@@ -10,8 +10,19 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-
-//= require swaggard/lib/base64
+//= require swaggard/lib/object-assign-pollyfill
 //= require swaggard/lib/jquery-1.8.0.min
-//= require swaggard/swagger-ui-bundle.js
-//= require swaggard/swagger-ui-standalone-preset.js
+//= require swaggard/lib/jquery.slideto.min
+//= require swaggard/lib/jquery.wiggle.min
+//= require swaggard/lib/jquery.ba-bbq.min
+//= require swaggard/lib/handlebars-4.0.5
+//= require swaggard/lib/lodash.min
+//= require swaggard/lib/backbone-min
+//= require swaggard/swagger-ui
+//= require swaggard/lib/highlight.9.1.0.pack
+//= require swaggard/lib/highlight.9.1.0.pack_extended
+//= require swaggard/lib/jsoneditor.min
+//= require swaggard/lib/marked
+//= require swaggard/lib/swagger-oauth
+//= require swaggard/translator
+//= require swaggard/swaggard
